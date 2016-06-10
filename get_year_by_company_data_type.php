ad778d85f8612be4badbc8df20a3ef12bd03cf06
Sayed Erfan Arefin <erfanjordison@gmail.com>
2016-06-10 19:24:55 +0600

    123

--- get_year_by_company_data_type.php
+++ get_year_by_company_data_type.php
@@ -2,19 +2,12 @@
 include 'db_connection.php';
 
 
-$Dept = $_POST['Dept'];
-$Dept_2 = $_POST['Dept_2'];
+$data = $_POST['Dept'];
+$company = $_POST['company'];
 
-/*
-     $query_current_semester = "SELECT company_name FROM companies WHERE company_type = '".$Dept."'";
-     $result_current_semester = mysqli_query($conn, $query_current_semester);
-     $row = $result_current_semester->fetch_assoc();
-      $semester = $row["Semester_name"];
-      //end of detecting current semester
+$data = strtolower(str_replace(' ', '_', $data));
 
-
-*/
-$query = "SELECT * FROM companies WHERE company_name = '".$Dept."'";
+$query = "SELECT DISTINCT(year) FROM ".$Dept_2." WHERE company_name = '".$Dept."'";
 
 $result = mysqli_query($conn, $query);
 
@@ -24,39 +17,9 @@ $i = 0;
 
 
 while($row = $result->fetch_assoc()){
-
-
-	if( $row["company_income"] != NULL){
-		$bullshit[$i] = "Income Statement";// $row["company_income"];
-    	$i++;
-	}
     
-    if( $row["company_balance"] != NULL){
-		$bullshit[$i] = "Balance Sheet";//$row["company_balance"];
-    	$i++;
-	}
-    
-    if( $row["company_cash"] != NULL){
-		$bullshit[$i] = "Cash Flow Statement";//$row["company_cash"];
-    	$i++;
-	}
-    
-    if( $row["company_equity"] != NULL){
-		$bullshit[$i] = "Statement of Equity";//$row["company_equity"];
-    	$i++;
-	}
-    
-    if( $row["company_liquidity"] != NULL){
-		$bullshit[$i] = "Liquidity Statement";//$row["company_liquidity"];
-    	$i++;
-	}
-    
-    if( $row["company_income_banks"] != NULL){
-		$bullshit[$i] = "Income Statement for Banks";//$row["company_income_banks"];
-    	$i++;
-	}
-    
-
+    $bullshit[$i] = $row["company_name"];
+    $i++;
 }
 
 $row_array = json_encode($bullshit);
